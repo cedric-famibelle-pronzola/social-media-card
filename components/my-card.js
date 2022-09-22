7391baf64cd9250fa789f0e076b9ea5018aa814d
Cédric FAMIBELLE-PRONZOLA <c.pronzola@tuta.io>
2022-09-22 08:40:39 +0400

    Create MyCard component

---
+++ components/my-card.js
@@ -0,0 +1,93 @@
+import Card from '@mui/material/Card'
+import CardMedia from '@mui/material/CardMedia'
+import CardContent from '@mui/material/CardContent'
+import Typography from '@mui/material/Typography'
+import Container from '@mui/material/Container'
+import Divider from '@mui/material/Divider'
+import Fab from '@mui/material/Fab'
+import Link from '@mui/material/Link'
+import List from '@mui/material/List'
+import useMediaQuery from '@mui/material/useMediaQuery'
+import KeyboardDoubleArrowDownIcon from '@mui/icons-material/KeyboardDoubleArrowDown'
+
+import data from '../social-media-card-data.json'
+
+import SocialMediaCardContent from '@/components/social-media-card-content'
+
+const USERNAME = process.env.NEXT_PUBLIC_USERNAME
+const EMAIL = process.env.NEXT_PUBLIC_EMAIL
+const AVATAR = process.env.NEXT_PUBLIC_AVATAR_FILENAME || 'avatar.png'
+const BIOGRAPHY = process.env.NEXT_PUBLIC_BIOGRAPHY
+
+function MyCard() {
+  const isMobile = useMediaQuery('(max-width:600px)')
+
+  const handleClick = event => {
+    const anchor = (event.target.ownerDocument).querySelector(
+      '#page-bottom'
+    )
+
+    if (anchor) {
+      anchor.scrollIntoView({
+        block: 'center',
+        behavior: 'smooth'
+      })
+    }
+  }
+
+  return (
+    <Container sx={{
+      padding: 0,
+      paddingTop: isMobile ? 0 : '5vh',
+      width: '100%',
+      marginBottom: isMobile ? 0 : 5
+    }} maxWidth='sm'
+    >
+      <Card sx={{minWidth: 150}}>
+        {USERNAME && (
+          <Typography sx={{mt: 2}} align='center' variant='h4'>
+            {USERNAME}
+          </Typography>
+        )}
+        <CardMedia
+          component='img'
+          sx={{width: 150, margin: 'auto', borderRadius: 2, mt: 2}}
+          image={`/images/${AVATAR}`}
+          alt='Avatar'
+        />
+        <CardContent>
+          {EMAIL && (
+            <Typography gutterBottom align='center' variant='body2'>
+              <Link underline='hover' href={`mailto:${EMAIL}`}>{EMAIL}</Link>
+            </Typography>
+          )}
+          {BIOGRAPHY && (
+            <Typography gutterBottom align='center' variant='body2' color='text.secondary'>
+              {BIOGRAPHY}
+            </Typography>
+          )}
+          <Divider sx={{mt: 2}}>
+            <Fab size='small' color='primary' aria-label='Scroll to bottom' onClick={handleClick}>
+              <KeyboardDoubleArrowDownIcon />
+            </Fab>
+          </Divider>
+          <Container sx={{mt: 2}} align='center' maxWidth='sm'>
+            <List
+              sx={{
+                width: '100%',
+                maxWidth: 360
+              }}
+            >
+              {data.map(({id, name, profilUrl, customIcon}) => (
+                <SocialMediaCardContent key={id} name={name} profilUrl={profilUrl} customIcon={customIcon} />
+              ))}
+            </List>
+            <div id='page-bottom' />
+          </Container>
+        </CardContent>
+      </Card>
+    </Container>
+  )
+}
+
+export default MyCard
