1cf4a95a50aa1ad7f0738c7c2d66f99504b1061b
Cédric FAMIBELLE-PRONZOLA <c.pronzola@tuta.io>
2022-09-22 08:32:21 +0400

    Import css files to _app

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,9 @@ import '@fontsource/roboto/300.css'
 import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/500.css'
 import '@fontsource/roboto/700.css'
+import 'fork-awesome/css/fork-awesome.min.css'
+
+import '../global.css'
 
 function SocialMediaCard({Component, pageProps}) {
   return (
