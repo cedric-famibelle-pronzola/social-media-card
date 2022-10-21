ed7fc5a2467ff4041bea4cf4283b70fba3339d7e
cedric-famibelle-pronzola <cedric@livingdata.co>
2022-10-21 05:30:40 +0000

    Deploying to public from @ cedric-famibelle-pronzola/social-media-card@2b1b43de3e38764c9cd07804f2b10ee350d712d4 🚀

---
+++ _next/static/j8tGDqDHlo8AbG_u8DsVB/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/385-9b5215a14a09e09e.js","static/chunks/pages/index-f7b5592ccdc7854e.js"],"/_error":["static/chunks/pages/_error-fb68742d3cf986b6.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
