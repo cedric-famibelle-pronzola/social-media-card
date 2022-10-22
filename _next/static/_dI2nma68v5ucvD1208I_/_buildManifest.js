f0f685ba9c35d5945ac8bd60a7337416315afa9f
cedric-famibelle-pronzola <cedric-famibelle-pronzola@users.noreply.github.com>
2022-10-22 20:49:31 +0000

    Deploying to public from @ cedric-famibelle-pronzola/social-media-card@0af809034eb6b6b4a0613c3ce0db7bf39fdef9ef 🚀

--- _next/static/_dI2nma68v5ucvD1208I_/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/382-bd4e0c66fdf85752.js","static/chunks/pages/index-a930c00c15cfe38f.js"],"/_error":["static/chunks/pages/_error-fb68742d3cf986b6.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
